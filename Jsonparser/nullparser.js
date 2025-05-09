9cd758db58da6510400c4dd1507c093f4f3794e3
Mohamed Aaqib <drop.mohamedaaqib@gmail.com>
2025-05-09 09:45:34 +0530

    implementing relp

--- Jsonparser/nullparser.js
+++
@@ -1,8 +0,0 @@
-function parseNull(input){
-    if (!input.startsWith("null"));
-    return null;
-    const remaining=input.slice(4);
-    return [null,remaining];
-}
-
-
--- Jsonparser/stringparser.js
+++ Jsonparser/stringparser.js
@@ -1,3 +1,13 @@
-function stringParser(input){
-  const regex = /^"+["\\\x00-\x1F]|()|()|()|()|()|()|()|()|()/;
+function stringParser(input) {
+  const regex = /^"+["\\\x00-\x1F]/;
+
+  let match = input.match(regex);
+  if (!match) {
+    return null;
+  }
+  let remaining = input.slice(match.length);
+
+  return [match, remaining];
+
+  console.log(stringParser("tsemongh$%%%"));
 }
